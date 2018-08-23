9e97315d35e385faa157245eed7c56a9f52b38f5
edgarovalle <eeovalle@gmail.com>
2018-08-22 19:53:50 -0500

    casi no HP

---
+++ script/jvaldez.js
@@ -0,0 +1,11 @@
+		$(document).ready(function(){
+		})
+
+		function bienvenida() {
+			var mensaje = 'Bienvenidos al';
+			alert(mensaje);
+		}
+
+		$('#btnMensaje').click(function(){
+			bienvenida();
+		})
